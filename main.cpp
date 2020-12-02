9286af23b8763747f5921752e621d56b19c13406
12Me21 <12Me21.mc@gmail.com>
2020-12-01 21:11:11 -0500

    what

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-#include <qapplication.h>
-#include <qpushbutton.h>
+#include <QMainWindow>
+#include <QApplication>
+#include <QPushButton>
+#include <stdio.h>
+
+void test() {
+	puts("test");	
+}
 
 int main(int argc, char **argv) {
-	QApplication a {argc, argv};
-	QPushButton hello {"Hello world!", 0};
-	hello.resize(100, 40);
-	hello.show();
-	return a.exec();
+	QApplication app {argc, argv};
+	QMainWindow window;
+	QPushButton hello {"Hello world!", &window};
+	QObject::connect(&hello, &QPushButton::released, &window, test);
+	
+	window.resize(100, 40);
+	window.show();
+	return app.exec();
 }
